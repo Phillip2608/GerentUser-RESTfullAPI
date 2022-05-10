142c0a6fe6c7646ca1a57470cb079b12a3212a12
Phillip2608 <luizlindo2416@gmail.com>
2022-05-10 15:49:30 -0300

    add methods post, delete, put and get with ajax request in the database

--- client-server/routes/users.js
+++ client-server/routes/users.js
@@ -9,14 +9,51 @@ var client = restify.createJsonClient({
 });
 
 /* GET users listing. */
-  router.get('/', function(req, res, next) {
+router.get('/', function(req, res, next) {
 
-    client.get('/users', function(err, request, response, obj) {
-      assert.ifError(err);
+  client.get('/users', function(err, request, response, obj) {
+    assert.ifError(err);
     
-      res.end(JSON.stringify(obj, null, 2));
-    });
+    res.json(obj);
   });
+});
+
+router.get('/:id', function(req, res, next) {
+
+  client.get(`/users/${req.params.id}`, function(err, request, response, obj) {
+    assert.ifError(err);
+    
+    res.end(JSON.stringify(obj, null, 2));
+  });
+});
+
+router.put('/:id', function(req, res, next) {
+
+  client.put(`/users/${req.params.id}`, req.body, function(err, request, response, obj) {
+    assert.ifError(err);
+    
+    res.end(JSON.stringify(obj, null, 2));
+  });
+});
+
+router.delete('/:id', function(req, res, next) {
+
+  client.del(`/users/${req.params.id}`, function(err, request, response, obj) {
+    assert.ifError(err);
+    
+    res.end(JSON.stringify(obj, null, 2));
+  });
+});
+
+router.post('/', function(req, res, next) {
+
+  client.put(`/users`, req.body, function(err, request, response, obj) {
+    assert.ifError(err);
+    
+    res.end(JSON.stringify(obj, null, 2));
+  });
+});
+
 
 
 module.exports = router;
